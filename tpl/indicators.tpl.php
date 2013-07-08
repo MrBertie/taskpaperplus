5e40140112f192eb5732748e765f9338294db450
Hendry Badao <hendrybadao@gmail.com>
2013-07-08 21:57:38 +0300

    New indicators file

---
+++ tpl/indicators.tpl.php
@@ -0,0 +1,7 @@
+<div id="indicators">
+    <span class="insert" id="insert" title="<?php echo \tpp\lang('insert_location_tip'); ?>">
+        <img id="top" src="images/insert_top.png" alt="insert" />
+        <img id="bottom" src="images/insert_bottom.png" alt="insert" />
+    </span>
+    <img id="sortable" src="images/sortable.png" alt='sortable'title="<?php echo \tpp\lang('sortable_tip'); ?>" />
+</div>
